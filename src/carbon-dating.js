4800a35352a246774ebff56d362cd3b14432b861
OlenaVoroshnina <113518916+OlenaVoroshnina@users.noreply.github.com>
2023-12-23 15:31:46 +0100

    feat: add code 2 tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,27 +1,43 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
+const LN2 = 0.693;
 
 /**
  * Determine the age of archeological find by using
  * given MODERN_ACTIVITY and HALF_LIFE_PERIOD values
- * 
- * @param {String} sampleActivity string representation of current activity 
+ *
+ * @param {String} sampleActivity string representation of current activity
  * @return {Number | Boolean} calculated age in years or false
  * in case of incorrect sampleActivity
  *
  * @example
- * 
+ *
  * dateSample('1') => 22387
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+   // Check if sampleActivity is a string and represents a valid number
+  if (typeof sampleActivity !== 'string' || !/^\d+(\.\d+)?$/.test(sampleActivity)) {
+    return false;
+  }
+
+  // Convert sampleActivity to a floating-point number
+  const activity = parseFloat(sampleActivity);
+
+  // Check if the activity is within a valid range
+  if (activity <= 0 || activity >= MODERN_ACTIVITY) {
+    return false;
+  }
+
+  // Calculate the age using the formula
+  const age = Math.ceil((Math.log(MODERN_ACTIVITY / activity) / LN2) * HALF_LIFE_PERIOD);
+
+  return age;
 }
 
 module.exports = {
-  dateSample
+  dateSample,
 };
--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  const dnsStats = {};
+
+  for (const domain of domains) {
+    const parts = domain.split('.').reverse();
+    let currentKey = '';
+
+    for (const part of parts) {
+      currentKey += `.${part}`;
+
+      dnsStats[currentKey] = (dnsStats[currentKey] || 0) + 1;
+    }
+  }
+
+  return dnsStats;
 }
 
 module.exports = {
